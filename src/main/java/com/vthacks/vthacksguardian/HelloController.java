a3813f6879aa1d227780e4650c07c64ac3fdbcf8
devd254 <devond156@gmail.com>
2022-11-12 16:20:51 -0500

    added function buttons

--- src/main/java/com/vthacks/vthacksguardian/HelloController.java
+++ src/main/java/com/vthacks/vthacksguardian/HelloController.java
@@ -11,4 +11,16 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    protected void onFileScanButtonClick() {
+        welcomeText.setText("File Scan!!!"); //temp
+    }
+    @FXML
+    protected void onSocialTraceButtonClick() {
+        welcomeText.setText("Social Media Trace!!!"); //temp
+    }
+    @FXML
+    protected void onEmailButtonClick() {
+        welcomeText.setText("Email Security!!!"); //temp
+    }
 }
