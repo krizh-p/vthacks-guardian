4ff9e98cafdb6a5d262a48a76a5cdc1e019112c8
krizh-p <kpurohit@gmu.edu>
2022-11-12 17:46:41 -0500

    implemented Test api call

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.vthacks.vthacksguardian {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires json.simple;
 
     opens com.vthacks.vthacksguardian to javafx.fxml;
     exports com.vthacks.vthacksguardian;
