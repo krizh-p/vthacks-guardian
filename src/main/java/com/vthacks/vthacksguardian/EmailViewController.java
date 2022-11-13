0dae7fb6406af2a3394c8c976762d903abbb3b0e
krizh-p <kpurohit@gmu.edu>
2022-11-12 19:46:44 -0500

    updated EmailView.java

--- src/main/java/com/vthacks/vthacksguardian/EmailViewController.java
+++ src/main/java/com/vthacks/vthacksguardian/EmailViewController.java
@@ -1,2 +1,16 @@
-package com.vthacks.vthacksguardian;public class EmailViewController {
+package com.vthacks.vthacksguardian;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class EmailViewController {
+
+    @FXML
+    private Button emailRunButton;
+
+    @FXML
+    private AnchorPane emailAnchor;
+
+
 }
