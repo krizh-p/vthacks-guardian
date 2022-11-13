142a3415868538e260152c6a244d2518aeb7a963
VanBlev <vanblev2003@gmail.com>
2022-11-12 21:42:40 -0500

    Added EmailController class with emailTextInput, emailRunButton, and emailAnchor. Added textField and button to email-view.fxml
    Imported the classes to allow the variables to work in the controller
    
    Added the background image to email-view.fxml, moved around the button and text prompt
    
    Created the email-view-results.fxml file as well as the EmailViewResultsController class

---
+++ src/main/java/com/vthacks/vthacksguardian/EmailViewResultsController.java
@@ -0,0 +1,18 @@
+package com.vthacks.vthacksguardian;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class EmailViewResultsController {
+
+    @FXML
+    private AnchorPane anchorEmailViewResults;
+
+    @FXML
+    private ImageView backgroundViewResults;
+
+}
