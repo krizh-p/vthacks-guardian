16fd2216bc5cc74e97424bfd2075c00fb14a284a
VanBlev <vanblev2003@gmail.com>
2022-11-12 19:44:05 -0500

    Added EmailController class with emailTextInput, emailRunButton, and emailAnchor. Added textField and button to email-view.fxml

---
+++ src/main/java/com/vthacks/vthacksguardian/EmailController.java
@@ -0,0 +1,17 @@
+package com.vthacks.vthacksguardian;
+
+import javafx.scene.control.TextField;
+
+public class EmailController {
+
+    @FXML
+    private TextField emailTextInput;
+
+    @FXML
+    private Button emailRunButton;
+
+    @FXML
+    private AnchorPane emailAnchor;
+
+
+}
