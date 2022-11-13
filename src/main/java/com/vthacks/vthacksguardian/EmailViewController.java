7c94146bb0a1465fabd601fdb436fedaecaae26a
krizh-p <kpurohit@gmu.edu>
2022-11-12 22:42:05 -0500

    implemented EmailViewController as Initilizable

--- src/main/java/com/vthacks/vthacksguardian/EmailViewController.java
+++ src/main/java/com/vthacks/vthacksguardian/EmailViewController.java
@@ -1,23 +1,79 @@
 package com.vthacks.vthacksguardian;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 
-public class EmailViewController {
+public class EmailViewController implements Initializable {
 
+    @FXML
+    private Button emailRunButton;
     @FXML
     private TextField emailTextInput;
-
     @FXML
     private AnchorPane emailAnchor;
-
     @FXML
     private ImageView backgroundEmailView;
 
 
+    @Override
+    public void initialize(URL currUrl, ResourceBundle resourceBundle) {
+        String str = emailRunButton.getText();
+        try {
+            str = str.replace("@", "%40");
+            URL url = new URL("https://emailrep.io/" + str);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET"); //requests
+            conn.setRequestProperty("accept", "application/json"); //header
+            conn.connect();
+
+            //Check if connect is made
+            int responseCode = conn.getResponseCode();
+
+            // 200 OK
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            } else {
+
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    informationString.append(scanner.nextLine());
+                }
+                //Close the scanner
+                scanner.close();
+
+                System.out.println("Parsed Data: "  + informationString);
+
+
+                //JSON simple library Setup with Maven is used to convert strings to JSON
+                JSONParser parse = new JSONParser();
+                JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
+                JSONObject data = (JSONObject) dataObject.get(0);
+
+                //Get the first JSON object in the JSON array
+                System.out.println(dataObject.get(0));
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/com/vthacks/vthacksguardian/HelloApplication.java
+++ src/main/java/com/vthacks/vthacksguardian/HelloApplication.java
@@ -8,10 +8,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static Stage stage;
+    public static Scene scene;
+
     @Override
     public void start(Stage stage) throws IOException {
+        HelloApplication.stage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 540);
+        HelloApplication.scene = scene;
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
